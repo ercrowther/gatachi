efbdd8fd0ec40254f60abad94a0eb968338fd3b9
Evan Crowther <evanccrowther@gmail.com>
2025-07-18 23:31:56 -0400

    Index.js starting code

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// This code is ran once the client is ready
+client.once(Events.ClientReady, readyClient => {
+	console.log(`✅ Client is ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to discord using token
+client.login(process.env.DISCORD_TOKEN);
