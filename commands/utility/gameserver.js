a6924bb4d98bb5f8f23715c1a8e3801fc9b56e84
Evan Crowther <evanccrowther@gmail.com>
2025-07-21 01:07:34 -0400

    Gameserverstatus command development contuation, ephemeral option added

--- commands/utility/gameserver.js
+++ commands/utility/gameserver.js
@@ -4,7 +4,14 @@ module.exports = {
     cooldown: 10,
     data: new SlashCommandBuilder()
         .setName("gameserverstatus")
-        .setDescription("Get the current status of the active DW Game Server"),
+        .setDescription("Get the current status of the current DW Game Server")
+        .addBooleanOption((option) =>
+            option
+                .setName("secret")
+                .setDescription(
+                    "When true, the status will only be visible for you"
+                )
+        ),
     async execute(interaction) {
         await interaction.reply("Pong");
     },
