941becbf07c458d9d476e7f8742b4ea6fb183c46
Evan Crowther <evanccrowther@gmail.com>
2025-07-27 19:04:51 -0400

    Added a new field to server config table

--- modules/database/models/ServerConfig.js
+++ modules/database/models/ServerConfig.js
@@ -4,6 +4,7 @@ const sequelize = require("../db");
 // guild_id: The ID for the discord server the config is for
 // alarm_role_id: The role ID for the GAT ALARM role
 // gameserver_role_id: The role ID for the GAME SERVER ACCESS role
+// alarm_sticky_state: The true or false state for if the gat alarm sticky message is active or not
 const ServerConfig = sequelize.define("serverconfig", {
     guild_id: {
         type: Sequelize.STRING,
@@ -16,6 +17,11 @@ const ServerConfig = sequelize.define("serverconfig", {
     gameserver_role_id: {
         type: Sequelize.STRING,
         defaultValue: null,
+    },
+    alarm_sticky_state: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
     },
 });
 
