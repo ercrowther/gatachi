067d0ea1d4f077ad1d9e1e4780b3ceb3c5d593aa
Evan Crowther <evanccrowther@gmail.com>
2025-07-27 15:18:36 -0400

    Added some comments to the serverConfig model

--- modules/database/db.js
+++ modules/database/db.js
@@ -6,7 +6,6 @@ const sequelize = new Sequelize("database", "user", "password", {
     host: "localhost",
     dialect: "sqlite",
     logging: false,
-    // SQLite only
     storage: "database.sqlite",
 });
 
--- modules/database/models/ServerConfig.js
+++ modules/database/models/ServerConfig.js
@@ -1,6 +1,9 @@
 const { Sequelize } = require("sequelize");
 const sequelize = require("../db");
 
+// guild_id: The ID for the discord server the config is for
+// alarm_role_id: The role ID for the GAT ALARM role
+// gameserver_role_id: The role ID for the GAME SERVER ACCESS role
 const ServerConfig = sequelize.define("serverconfig", {
     guild_id: Sequelize.STRING,
     alarm_role_id: Sequelize.STRING,
