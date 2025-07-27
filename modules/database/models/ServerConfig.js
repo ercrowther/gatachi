89c1def1abff8ad10afe2b1d837b6c0e3e8b8458
Evan Crowther <evanccrowther@gmail.com>
2025-07-27 15:35:25 -0400

    Made the server config table's fields default to null

--- modules/database/models/ServerConfig.js
+++ modules/database/models/ServerConfig.js
@@ -5,9 +5,18 @@ const sequelize = require("../db");
 // alarm_role_id: The role ID for the GAT ALARM role
 // gameserver_role_id: The role ID for the GAME SERVER ACCESS role
 const ServerConfig = sequelize.define("serverconfig", {
-    guild_id: Sequelize.STRING,
-    alarm_role_id: Sequelize.STRING,
-    gameserver_role_id: Sequelize.STRING,
+    guild_id: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+    },
+    alarm_role_id: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+    },
+    gameserver_role_id: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+    },
 });
 
 module.exports = ServerConfig;
