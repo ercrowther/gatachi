5ffbc09558996957af48f40a4d48346daf159a7c
Evan Crowther <evanccrowther@gmail.com>
2025-08-03 23:28:47 -0400

    Added event handler for messageCreate event

---
+++ events/messageCreate.js
@@ -0,0 +1,8 @@
+const { Events } = require("discord.js");
+
+module.exports = {
+    name: Events.MessageCreate,
+    execute(message) {
+        // TODO: call a handler's function to do alarm sticky pin logic
+    },
+};
