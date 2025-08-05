5234f26c8e514bd62525ea3e2791cba57780b20b
Evan Crowther <evanccrowther@gmail.com>
2025-08-04 22:00:31 -0400

    Handled a case where bot may shutdown during an active alarm by resetting all alarm states back to false

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,17 @@
 const { Events } = require("discord.js");
 // Require database models
 const ServerConfigModel = require("../modules/database/models/serverConfig");
+const crudHandler = require("../modules/database/crudHandler");
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
-    execute(client) {
+    async execute(client) {
         ServerConfigModel.sync();
+
+        // Handle a case where the bot shutdown during an active sticky pin
+        await crudHandler.resetAlarmStatesForAllServerConfigs();
+
         console.log(`✅ Client is ready! Logged in as ${client.user.tag}`);
     },
 };
--- modules/eventFeatures/alarmStickyPinHandler.js
+++ modules/eventFeatures/alarmStickyPinHandler.js
@@ -1,12 +1,16 @@
 const crudHandler = require("../database/crudHandler");
 
-function handleStickyPin(message) {
+async function handleStickyPin(message) {
     // Is the sticky pin status set to false?
     // If yes, does the message mention the alarm? if not, return early
     // If the message does mention the alarm, set sticky pin status to true and send the message
     // . . .
     // Is the sticky pin status set to true?
     // If yes, get the message id of the last sent message, delete it, and resend it
+}
+
+function sendEmbedPin(channelId) {
+    channelId.send("test message");
 }
 
 module.exports = { handleStickyPin };
