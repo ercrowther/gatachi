b326de244ad456788342a67f0e1802f91d365288
Evan Crowther <evanccrowther@gmail.com>
2025-08-06 14:19:20 -0400

    Implemented batching using array destructuring and promise all to query the database in parallel for necessary information that is needed for each part of the logic, alarm true or not

--- modules/eventFeatures/alarmStickyPinHandler.js
+++ modules/eventFeatures/alarmStickyPinHandler.js
@@ -13,19 +13,21 @@ async function handleStickyPin(message) {
     const client = message.client;
     // Message related variables
     const channel = client.channels.cache.get(message.channelId);
-    const alarmRoleId = await crudHandler.fetchAlarmRoleId(guildId);
 
     try {
+        // Batching database queries for necessary information
+        const [alarmRoleId, alarmChannelId, isAlarmActive] = Promise.all([
+            await crudHandler.fetchAlarmRoleId(guildId),
+            await crudHandler.fetchAlarmMessageChannelID(guildId),
+            await crudHandler.fetchAlarmStickyState(guildId),
+        ]);
+
         // If there is an active alarm and the user isn't typing in that channel, return immediately
-        const alarmChannelId = await crudHandler.fetchAlarmMessageChannelID(
-            guildId
-        );
         if (alarmChannelId && message.channelId != alarmChannelId) {
             return;
         }
 
         // Check if there is an active pin. If not, set the state to active
-        const isAlarmActive = await crudHandler.fetchAlarmStickyState(guildId);
         if (!isAlarmActive) {
             // If alarm mentioned, set state to true, otherwise return early
             if (message.mentions.roles.has(alarmRoleId)) {
@@ -43,11 +45,15 @@ async function handleStickyPin(message) {
         await sendEmbedPin(channel, guildId);
     } catch (error) {
         console.log(`❌ ERROR: ${error}`);
-        resetStatesAndValuesToDefault(guildId);
+
+        await resetStatesAndValuesToDefault(guildId).catch((error2) => {
+            console.log(`❌ ERROR: ${error2}`);
+        });
     }
 }
 
 async function sendEmbedPin(channel, guildId) {
+    // Send the message and additionally update the latest message id and alarm channel id
     channel.send("test message").then(async (sentMessage) => {
         await crudHandler.updateAlarmMessageID(guildId, sentMessage.id);
         await crudHandler.updateAlarmChannelID(guildId, channel.id);
@@ -65,6 +71,7 @@ async function removePreviousMessage(channel, guildId) {
 }
 
 async function resetStatesAndValuesToDefault(guildId) {
+    // Reset all alarm values to defaults
     await crudHandler.updateAlarmMessageID(guildId, null);
     await crudHandler.updateAlarmChannelID(guildId, null);
     await crudHandler.updateAlarmStickyStatus(guildId, false);
