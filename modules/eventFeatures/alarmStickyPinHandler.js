595b2d85d60851022fee201e5e06f463fabf8a28
Evan Crowther <evanccrowther@gmail.com>
2025-08-06 16:20:29 -0400

    Fixed typo

--- modules/eventFeatures/alarmStickyPinHandler.js
+++ modules/eventFeatures/alarmStickyPinHandler.js
@@ -79,7 +79,7 @@ async function sendEmbedPin(channel, guildId) {
         .setColor("#ffac32")
         .setTitle("ACTIVE GAT ALARM!")
         .setDescription(
-            "Your help is needed!!! Click the blue text to jump to the game servers! \n \n Joining? Send a reaction to this message to be included in the list of participants at the end! (don't worry if you're reaction disappears, you only need to react once!)"
+            "Your help is needed!!! Click the blue text to jump to the game servers! \n \n Joining? Send a reaction to this message to be included in the list of participants at the end! (don't worry if your reaction disappears, you only need to react once!)"
         )
         .setThumbnail(process.env.ALARM_ICON_URL)
         .addFields({
