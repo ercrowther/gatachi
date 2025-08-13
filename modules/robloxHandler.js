f10cc00bbcd38369428140877907e3f8ee25ad51
Evan Crowther <evanccrowther@gmail.com>
2025-08-12 23:19:28 -0400

    Created a new handler for ROBLOX api requests. Implemented a function to retrieve friend's from a user

---
+++ modules/robloxHandler.js
@@ -0,0 +1,65 @@
+/**
+ * Fetches the roblox user id's that a specified user has friended
+ *
+ * This function utilizes the ROBLOX v1 friend endpoint, making batch calls of 50 friends
+ * per 1 second to prevent rate limiting
+ *
+ * @param {number} userId - The ROBLOX id of the user whose friend's to get
+ * @returns {Promise<Set<number>>} A promise that resolves into a set of userId's that the given user is friended to
+ * @throws {Error} Throws an error if any of the fetches fail
+ */
+async function returnUsersFriends(userId) {
+    // The id's of the user's friends
+    const friendIDs = new Set();
+    // The url to use for the next fetch operation
+    let url = `https://friends.roblox.com/v1/users/${userId}/friends/find?limit=50&cursor=&userSort=`;
+    let response = await fetch(url);
+    let data = await response.json();
+    // Flag for if the fetching should continue
+    let hasMore = true;
+
+    // Stop after parsing current page if there is no next page
+    while (hasMore) {
+        if (response.status != 200) {
+            throw new Error("Failed to fetch roblox user's friends");
+        }
+
+        // Add each id on the current page to the set
+        for (const item of data.PageItems) {
+            if (item?.id) {
+                friendIDs.add(item.id);
+            }
+        }
+
+        // If there is another page of data
+        if (data.NextCursor) {
+            url = `https://friends.roblox.com/v1/users/${userId}/friends/find?limit=50&cursor=${data.NextCursor}&userSort=`;
+
+            // Sleep 1 second per batch of 50 friends to prevent ratelimiting
+            await sleep(1000);
+            response = await fetch(url);
+            data = await response.json();
+        } else {
+            hasMore = false;
+        }
+    }
+
+    return friendIDs;
+}
+
+/**
+ * Pause execution for a specified amount of miliseconds
+ *
+ * This function returns a promise after a certain amnount of miliseconds, letting the caller of
+ * this function use await to await that many seconds
+ *
+ * @param {number} ms - The delay duration in milliseconds.
+ * @returns {Promise<void>} A Promise that resolves after the delay.
+ */
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+module.exports = {
+    returnUsersFriends,
+};
