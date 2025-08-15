99cd2d9af1bed0372a3382fce837df8445fda938
Evan Crowther <evanccrowther@gmail.com>
2025-08-15 13:35:18 -0400

    Implemented addFlaggedUser command

--- commands/database/addFlaggedUser.js
+++ commands/database/addFlaggedUser.js
@@ -19,5 +19,52 @@ module.exports = {
                 .setRequired(true)
         )
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
-    async execute(interaction) {},
+    async execute(interaction) {
+        const username = interaction.options.getString("username");
+        let userId = 0;
+
+        // Guard clause for making sure the username links to a valid roblox profile
+        try {
+            userId = await robloxHandler.getIDByUsername(username);
+        } catch {
+            // Send a meaningful message
+            const badUserEmbed = new EmbedBuilder()
+                .setDescription("**This username does not exist on ROBLOX!")
+                .setColor("#fc0303");
+            await interaction.reply({
+                embeds: [badUserEmbed],
+                ephemeral: true,
+            });
+
+            return;
+        }
+
+        try {
+            await crudHandler.createFlaggedUser(userId, username);
+
+            const successEmbed = new EmbedBuilder()
+                .setColor("#10b91f")
+                .setDescription("Successfully added account to list of flagged users!");
+            await interaction.reply({
+                embeds: [replyEmbed],
+                ephemeral: true,
+            });
+
+            await interaction.reply({
+                embeds: [successEmbed],
+                ephemeral: true,
+            });
+        } catch (error) {
+            // Send a meaningful message
+            const errorEmbed = new EmbedBuilder()
+                .setDescription(`**FATAL ERROR** - ${error}`)
+                .setColor("#fc0303");
+            await interaction.reply({
+                embeds: [errorEmbed],
+                ephemeral: true,
+            });
+
+            return;
+        }
+    },
 };
