4cc89e0121470899334a71df7a78a23a04ed0b8d
Evan Crowther <evanccrowther@gmail.com>
2025-08-19 15:38:36 -0400

    Added two helper functions to assist in extracting usernames

--- commands/utility/joinme.js
+++ commands/utility/joinme.js
@@ -9,3 +9,26 @@ module.exports = {
         ),
     async execute(interaction) {},
 };
+
+/**
+ * Retrieve the ROBLOX username from a guild member's nickname
+ *
+ * @param {string} nickname - The discord nickname of a guild member
+ * @returns {string|null} A string containing the ROBLOX username of the member, otherwise null
+ */
+function extractUsername(nickname) {
+    const match = nickname.match(/\(([^)]+)\)$/);
+    return match ? match[1] : null;
+}
+
+/**
+ * Retrieve the preferred name of a guild member's nickname. Useful to fall back on if
+ * the ROBLOX username doesn't exist
+ *
+ * @param {string} nickname - The discord nickname of a guild membe
+ * @returns {string|null} A string containing the preferred name of the member, otherwise null
+ */
+function extractNick(nickname) {
+    const match = nickname.match(/^(.*?)\s*\(/);
+    return match ? match[1].trim() : null;
+}
