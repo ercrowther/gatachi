d4e3a7443615f30843f263bc29b1724a20dfe033
Evan Crowther <evanccrowther@gmail.com>
2025-08-20 17:27:05 -0400

    Implemented logic for building pages of embeds

--- commands/database/flaggedUsers.js
+++ commands/database/flaggedUsers.js
@@ -6,6 +6,8 @@ const {
 const crudHandler = require("../../modules/database/crudHandler");
 const paginationHandler = require("../../modules/paginationHandler");
 
+const namesPerPage = 10;
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("flaggedusers")
@@ -33,3 +35,29 @@ module.exports = {
         await paginationHandler.paginate(interaction, pages);
     },
 };
+
+/**
+ * Build an array of embeds that hold a specified amount of names. Each element acts like a page
+ *
+ * @returns {EmbedBuilder[]} An array of embeds that act as pages
+ */
+async function buildPages() {
+    // Hold the names of each FlaggedUser until it is time to add it to a page
+    let currentPageInfo = "";
+    const users = await crudHandler.fetchAllFlaggedUsers();
+    const pages = [];
+
+    for (let i = 0; i < users.length; i++) {
+        currentPageInfo += `${users.name}\n`;
+
+        // If page is full or it’s the last item, add current info into the page
+        if ((i + 1) % pageSize === 0 || i === items.length - 1) {
+            pages.push(new EmbedBuilder().setDescription(currentPageInfo));
+
+            // Flush the current page info
+            currentPageInfo = "";
+        }
+    }
+
+    return pages;
+}
