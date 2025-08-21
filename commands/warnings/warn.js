4f8c3cc3a07f13801050770a9504de082dbfab9b
Evan Crowther <evanccrowther@gmail.com>
2025-08-20 20:59:25 -0400

    Fixed issue with warn command declaration

--- commands/warnings/warn.js
+++ commands/warnings/warn.js
@@ -9,26 +9,28 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("warn")
         .setDescription("ADMIN ONLY. Give out a warning to a member")
-        .addMentionableOption((option) => {
+        .addMentionableOption((option) =>
             option
                 .setName("user")
                 .setDescription("The member to warn")
-                .setRequired(true);
-        })
-        .addStringOption((option) => {
+                .setRequired(true)
+        )
+        .addStringOption((option) =>
             option
                 .setName("reason")
-                .setDescription("Optional - The reason for the warning");
-        })
-        .addIntegerOption((option) => {
+                .setDescription("Optional - The reason for the warning")
+        )
+        .addIntegerOption((option) =>
             option
                 .setName("severity")
                 .setDescription(
                     "Optional - How severe the warning is between 1-5"
                 )
                 .setMinValue(1)
-                .setMaxValue(5);
-        })
+                .setMaxValue(5)
+        )
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
-    async execute(interaction) {},
+    async execute(interaction) {
+        console.log(interaction.guildId);
+    },
 };
