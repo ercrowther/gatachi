81792cec6ea0702ee165a119f7ee64285fb01f46
Evan Crowther <evanccrowther@gmail.com>
2025-08-20 20:55:09 -0400

    Defined a new command to give out a warning to a specified user

---
+++ commands/warnings/warn.js
@@ -0,0 +1,34 @@
+const {
+    SlashCommandBuilder,
+    EmbedBuilder,
+    PermissionFlagsBits,
+} = require("discord.js");
+const crudHandler = require("../../modules/database/crudHandler");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("warn")
+        .setDescription("ADMIN ONLY. Give out a warning to a member")
+        .addMentionableOption((option) => {
+            option
+                .setName("user")
+                .setDescription("The member to warn")
+                .setRequired(true);
+        })
+        .addStringOption((option) => {
+            option
+                .setName("reason")
+                .setDescription("Optional - The reason for the warning");
+        })
+        .addIntegerOption((option) => {
+            option
+                .setName("severity")
+                .setDescription(
+                    "Optional - How severe the warning is between 1-5"
+                )
+                .setMinValue(1)
+                .setMaxValue(5);
+        })
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    async execute(interaction) {},
+};
