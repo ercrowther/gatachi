f6d9508b7da3abcde8b8c6a0902de0c46eaf8aba
Evan Crowther <evanccrowther@gmail.com>
2025-08-21 16:10:40 -0400

    Added build page helper function and logic for sending a pagniated embed for when a user is mentioned

--- commands/warnings/warnings.js
+++ commands/warnings/warnings.js
@@ -4,6 +4,9 @@ const {
     PermissionFlagsBits,
 } = require("discord.js");
 const crudHandler = require("../../modules/database/crudHandler");
+const paginationHandler = require("../../modules/paginationHandler");
+
+const warnsPerPage = 5;
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -54,10 +57,82 @@ module.exports = {
     async execute(interaction) {
         const guildId = interaction.guildId;
         // Parse options
-        const targetId = interaction.options.getUser("user")?.id;
+        const target = interaction.options.getUser("user");
+        const targetId = target?.id;
         const sortOption =
             interaction.options.getString("sort_by") ?? undefined;
         const orderOption =
             interaction.options.getString("order_by") ?? undefined;
+
+        // Show user specific warnings
+        if (targetId) {
+            const userWarns = await crudHandler.fetchWarnings(
+                guildId,
+                sortOption,
+                orderOption,
+                targetId
+            );
+
+            // Send a paginated embed
+            const userWarnPages = buildPages(userWarns, target);
+            await paginationHandler.paginate(interaction, userWarnPages);
+
+            return;
+        }
     },
 };
+
+/**
+ * Build an array of embeds that hold a specified amount of warnings. Each element acts like a page
+ *
+ * @param {Object[]} warnings - An array of Warning objects
+ * @param {GuildMember} targetId - An optional parameter for a guild member
+ * @returns {EmbedBuilder[]} An array of embeds that act as pages
+ */
+function buildPages(warnings, target) {
+    // Hold the warnings until it is time to add it to a page
+    let currentPageInfo = "";
+    let pageCount = 1;
+    const pages = [];
+    console.log(warnings);
+
+    for (let i = 0; i < warnings.length; i++) {
+        // Parse a few datas from the element
+        const reason = warnings[i].dataValues.reasoning
+            ? warnings[i].dataValues.reasoning
+            : "No reason provided";
+        const date = new Date(warnings[i].dataValues.date);
+
+        // Add one line of warning info
+        currentPageInfo += `ID: \`${warnings[i].dataValues.warningId}\``;
+        if (!target) {
+            currentPageInfo += ` | ${target.username}`;
+        }
+        currentPageInfo += ` | Severity: \`${
+            warnings[i].dataValues.severity
+        }\`\n${reason} - \`${date.toString()}\`\n\n`;
+
+        // If page is full or it’s the last item, add current info into the page
+        if ((i + 1) % warnsPerPage === 0 || i === warnings.length - 1) {
+            pages.push(
+                new EmbedBuilder()
+                    .setDescription(currentPageInfo)
+                    .setColor("#10b91f")
+                    .setTitle(
+                        target ? `${target.username}'s Warnings` : "Warnings"
+                    )
+                    .setFooter({
+                        text: `Page ${pageCount} of ${Math.ceil(
+                            (warnings.length - 1) / warnsPerPage
+                        )}`,
+                    })
+            );
+            pageCount += 1;
+
+            // Flush the current page info
+            currentPageInfo = "";
+        }
+    }
+
+    return pages;
+}
