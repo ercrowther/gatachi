77762eced1b1d1b57f73ba458d3d70efca1fec85
Evan Crowther <evanccrowther@gmail.com>
2025-08-22 18:32:54 -0400

    Defined the victoryMention table

---
+++ modules/database/models/victoryMention.js
@@ -0,0 +1,26 @@
+const { Sequelize } = require("sequelize");
+const sequelize = require("../db");
+
+/**
+ * id: Normal primary key to uniquely identify each victory mention
+ * victoryId: A foreign key to a victory id (not a victory's victoryId, but their primary key)
+ * userId: The discord user id of the user being mentioned in a victory
+ */
+const VictoryMention = sequelize.define("victorymention", {
+    id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    victoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    userId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+});
+
+module.export = VictoryMention;
