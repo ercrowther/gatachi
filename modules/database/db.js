4b4b52645a4db99514ab688a74d24545deea195e
Evan Crowther <evanccrowther@gmail.com>
2025-08-22 18:35:38 -0400

    Defined the relationship between a victory and it's victory mentions

--- modules/database/db.js
+++ modules/database/db.js
@@ -1,5 +1,7 @@
 // Require Sequelize
 const Sequelize = require("sequelize");
+const Victory = require("../database/models/victory");
+const VictoryMention = require("../database/models/victoryMention");
 
 // Create a new sequelize instance to define connection information
 const sequelize = new Sequelize("database", "user", "password", {
@@ -8,5 +10,13 @@ const sequelize = new Sequelize("database", "user", "password", {
     logging: false,
     storage: "database.sqlite",
 });
+
+// Associations
+Victory.hasMany(VictoryMention, {
+    foreignKey: "victoryId",
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE",
+});
+VictoryMention.belongsTo(Victory, { foreignKey: "victoryId" });
 
 module.exports = sequelize;
