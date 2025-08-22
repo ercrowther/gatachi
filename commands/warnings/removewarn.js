92ba91be1b0c36d9a22e778e5c6f80853459bbdd
Evan Crowther <evanccrowther@gmail.com>
2025-08-21 20:08:09 -0400

    Added logic for deleting the warning

--- commands/warnings/removewarn.js
+++ commands/warnings/removewarn.js
@@ -19,8 +19,34 @@ module.exports = {
             option
                 .setName("warning_id")
                 .setDescription("The ID of the user's warning")
+                .setMinValue(1)
                 .setRequired(true)
         )
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
-    async execute(interaction) {},
+    async execute(interaction) {
+        // Get necessary data for deletion
+        const guildId = interaction.guildId;
+        const userId = interaction.options.getUser("user").id;
+        const warningId = interaction.options.getInteger("warning_id");
+
+        try {
+            await crudHandler.deleteWarning(guildId, userId, warningId);
+
+            const successEmbed = new EmbedBuilder().setDescription(
+                `Warning with ID \`${warningId}\` successfully deleted!`
+            );
+
+            await interaction.reply({ embeds: [successEmbed] });
+        } catch (error) {
+            // Send a meaningful message
+            const errorEmbed = new EmbedBuilder()
+                .setDescription(`${error}`)
+                .setColor("#fc0303");
+            await interaction.reply({
+                embeds: [errorEmbed],
+            });
+
+            return;
+        }
+    },
 };
