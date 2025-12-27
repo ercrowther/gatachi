1c0afdb3edbe1213a9c4d2697cc5494c9a798ea2
Evan Crowther <evanccrowther@gmail.com>
2025-12-26 23:10:14 -0500

    Added date index to speed up some queries

--- modules/database/models/victories.js
+++ modules/database/models/victories.js
@@ -10,40 +10,46 @@ const sequelize = require("../db");
  * imageUrl: The image url of a screenshot from the victory, if one exists
  * date: The date of the victory in the format 'yyyy-mm-dd'
  */
-const Victories = sequelize.define("victories", {
-    id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
+const Victories = sequelize.define(
+    "victories",
+    {
+        id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        victoryInternalId: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            unique: true,
+        },
+        ragequits: {
+            type: Sequelize.INTEGER,
+            defaultValue: 0,
+            allowNull: false,
+        },
+        standdowns: {
+            type: Sequelize.INTEGER,
+            defaultValue: 0,
+            allowNull: false,
+        },
+        terminations: {
+            type: Sequelize.INTEGER,
+            defaultValue: 0,
+            allowNull: false,
+        },
+        imageUrl: {
+            type: Sequelize.STRING,
+        },
+        date: {
+            type: Sequelize.DATEONLY,
+            allowNull: false,
+        },
     },
-    victoryInternalId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        unique: true,
-    },
-    ragequits: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0,
-        allowNull: false,
-    },
-    standdowns: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0,
-        allowNull: false,
-    },
-    terminations: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0,
-        allowNull: false,
-    },
-    imageUrl: {
-        type: Sequelize.STRING,
-    },
-    date: {
-        type: Sequelize.DATEONLY,
-        allowNull: false,
-    },
-});
+    {
+        indexes: [{ fields: ["date"] }],
+    }
+);
 
 module.exports = Victories;
