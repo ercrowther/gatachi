bc63ddab844564b1690eabe625faa358bc5e046d
Evan Crowther <evanccrowther@gmail.com>
2025-12-27 23:10:14 -0500

    Quick update to force a guild member cache warmup

--- events/ready.js
+++ events/ready.js
@@ -32,5 +32,29 @@ module.exports = {
         client.user.setActivity("the safety of DW", {
             type: ActivityType.Watching,
         });
+
+        // Warm the guildmembers cache
+        for (const guild of client.guilds.cache.values()) {
+            // Dont do this on a large guild its super slow
+            if (guild.memberCount > 2000) {
+                console.warn(
+                    `⚠️ Skipping member fetch for ${guild.name}. (${guild.memberCount} members!)`
+                );
+                continue;
+            }
+
+            try {
+                console.log(`🔄 Warming member cache for ${guild.name}...`);
+                await guild.members.fetch();
+                console.log(
+                    `✅ Cached ${guild.members.cache.size} members for ${guild.name}`
+                );
+            } catch (err) {
+                console.error(
+                    `❌ Failed to fetch members for ${guild.name}`,
+                    err
+                );
+            }
+        }
     },
 };
